c1c3ef284de4bbe88df793b125170929747e43b9
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-05-06 07:58:51 +0900

    Add width style.

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,5 +1,5 @@
 <template class="w-full h-full">
-  <article class="w-9/12 mx-auto p-8">
+  <article class="w-11/12 lg:w-9/12 mx-auto p-8">
     <h1 class="text-6xl text-center p-8">{{ blogs.title }}</h1>
     <nuxt-content :document="blogs" />
   </article>
