31b64dff270fbf5854105b0b0ca78fe2725e7a80
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-05-15 17:50:29 +0900

    Fix site design.

---
+++ components/footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="w-full text-center p-8">
+    <p>Copyright © 2021 Hikaru Kobayashi</p>
+  </footer>
+</template>
---
+++ components/header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="w-full h-16 lg:h-32 table shadow">
+    <h1 class="text-center lg:text-2xl font-bold table-cell align-middle">HikaBlog</h1>
+  </header>
+</template>
