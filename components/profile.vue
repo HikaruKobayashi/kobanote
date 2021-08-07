dd5d1583aa19c147f117050a1e2979ad0834c882
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-08-07 16:51:20 +0900

    Icon image settings.

--- components/profile.vue
+++ components/profile.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="flex py-6">
-    <div class="mr-auto">
-      <p>Kobayahi</p>
-    </div>
-    <div>
-      <a href="https://twitter.com/kobayahi0719"><fa class="fa-lg text-gray-500" :icon="['fab', 'twitter']" /></a>
-    </div>
+    <ul class="mr-auto">
+      <li class="inline-block align-middle">
+        <img class="w-12 h-12 rounded-full" src="@/assets/img/kobayahi.JPG" alt="kobayahi-img" />
+      </li>
+      <li class="inline-block align-middle">
+        <p>Kobayahi</p>
+      </li>
+    </ul>
+    <ul class="flex items-center justify-end">
+      <li>
+        <a href="https://twitter.com/kobayahi0719"><fa class="fa-lg text-gray-500" :icon="['fab', 'twitter']" /></a>
+      </li>
+    </ul>
   </div>
 </template>
