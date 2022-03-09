088a6872bfc3ea081adb55fd83ee08eaa8b35515
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-03-09 21:33:16 +0900

    edit header style

--- components/atoms/header/title.vue
+++ components/atoms/header/title.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full px-4 flex items-center">
-    <h1 class="lg:text-2xl font-bold text-center"><a href="/">kobanote</a></h1>    
+  <div class="w-11/12 lg:w-4/12 mx-auto p-4 flex items-center">
+    <h1 class="lg:text-2xl font-bold text-center"><a href="/">kobanote</a></h1>
   </div>
 </template>
